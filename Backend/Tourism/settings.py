34f87cd849a173d34a71ba92d7b23da61252efb6
Julius Khamara <khamarajulius@gmail.com>
2024-06-23 02:14:12 +0000

    installed react and ensured the backend(django) can communicate with the frontend(react) using django-cors-headers.

--- Backend/Tourism/settings.py
+++ Backend/Tourism/settings.py
@@ -26,10 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'home',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -37,6 +39,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000/'
 ]
 
 ROOT_URLCONF = 'Tourism.urls'
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>Group 2 Tourism app</div>
+    </div>
+  );
+}
+
+export default App;
