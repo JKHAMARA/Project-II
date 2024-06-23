f8637c262fc51a692f4c0d747c2b6877e7a3a490
Julius Khamara <khamarajulius@gmail.com>
2024-06-23 03:28:56 +0000

    Added our pages created in components to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Tours from './components/Tours';
+import Create from './components/Create';
 
 function App() {
   return (
     <div className="App">
-     <div>Group 2 Tourism app</div>
+      <Routes>
+        <Route path="" element={<Home/>}/>
+        <Route path="/tours" element={<Tours/>}/>
+        <Route path="/create" element={<Create/>}/>
+      </Routes>
     </div>
   );
 }
