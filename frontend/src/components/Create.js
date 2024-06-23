b0585bbf7e93d800d5fa78f60b46fc0c1fd6de17
Julius Khamara <khamarajulius@gmail.com>
2024-06-23 03:30:23 +0000

    Pages created in our components directory

---
+++ frontend/src/components/Create.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Create = () => {
+    return (
+        <div>Create Page</div>
+    )
+}
+export default Create
---
+++ frontend/src/components/Tours.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Tours = () => {
+    return (
+        <div>Tours</div>
+    )
+}
+export default Tours
